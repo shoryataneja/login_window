3cce64e35692f67b8d17264105f16c7fca6caca3
shoryataneja <shoryataneja5@gmail.com>
2026-05-14 10:34:06 +0530

    add Google OAuth with Passport.js

---
+++ backend/config/passport.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (!user) {
+          user = await User.findOne({ email: profile.emails[0].value });
+          if (user) {
+            user.googleId = profile.id;
+            if (!user.avatar) user.avatar = profile.photos[0]?.value;
+            await user.save();
+          } else {
+            user = await User.create({
+              name: profile.displayName,
+              email: profile.emails[0].value,
+              googleId: profile.id,
+              avatar: profile.photos[0]?.value,
+              isVerified: true,
+            });
+          }
+        }
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
+      }
+    }
+  )
+);
+
+module.exports = passport;
